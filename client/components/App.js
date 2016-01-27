19d0192f7707bde56c584311bbec03113f5fdd00
pavelmp <pavelmp@gmail.com>
2016-01-27 15:54:35 -0800

    Enable video data to display through VideoListEntry component

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,9 @@ var App = () => (
       <VideoPlayer/>
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList videoCollection = {window.exampleVideoData}/>
     </div>
   </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,14 +1,14 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
+    <VideoListEntry video = {props.videoCollection[0]}/>
+    <VideoListEntry video = {props.videoCollection[1]}/>
+    <VideoListEntry video = {props.videoCollection[2]}/>
+    <VideoListEntry video = {props.videoCollection[3]}/>
+    <VideoListEntry video = {props.videoCollection[4]}/>
+    {/*<VideoListEntry />
     <VideoListEntry />
     <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    <VideoListEntry />*/}
   </div>
 );
 
