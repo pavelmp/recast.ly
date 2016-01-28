aab6e02fe00addb78329ba2ab171d104530267b7
pavelmp <pavelmp@gmail.com>
2016-01-27 16:10:34 -0800

    Create a dynamic video player component

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,7 +1,7 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">{props.video.snippet.title}</div>
