a8e8010dd23588ef3e858e702ae7297cca7652dc
pavelmp <pavelmp@gmail.com>
2016-01-27 18:57:29 -0800

    Add clickHandler functionality

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,4 @@
-var App = () => (
+{/*var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
@@ -10,4 +10,32 @@ var App = () => (
   </div>
 );
 
+ReactDOM.render(<App />, document.getElementById('app'));*/}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {videoCollection: window.exampleVideoData,
+                  currentVideo: window.exampleVideoData[0]};
+  }
+
+  handleClick() {
+    this.setState({currentVideo: window.exampleVideoData[1]})
+    console.log('success!')
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer  curState = {this.state}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList onClick={this.handleClick.bind(this)} curState = {this.state}/>
+        </div>
+      </div>);
+  }  
+}
+
+//window.app = new App();
 ReactDOM.render(<App />, document.getElementById('app'));
--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,11 +1,11 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry video = {props.videoCollection[0]}/>
-    <VideoListEntry video = {props.videoCollection[1]}/>
+    <VideoListEntry onClick={props.onClick.bind(this)} curState = {props.curState}/>
+   {/*} <VideoListEntry video = {props.videoCollection[1]}/>
     <VideoListEntry video = {props.videoCollection[2]}/>
     <VideoListEntry video = {props.videoCollection[3]}/>
     <VideoListEntry video = {props.videoCollection[4]}/>
-    {/*<VideoListEntry />
+    <VideoListEntry />
     <VideoListEntry />
     <VideoListEntry />
     <VideoListEntry />*/}
